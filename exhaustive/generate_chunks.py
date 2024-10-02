971a489a926f3b8adc4dc6ffd4f1889a3eee0abe
hardyek <hardyewk@gmail.com>
2024-10-02 21:25:11 +0100

    exhaustive all chunks generated.

---
+++ exhaustive/generate_chunks.py
@@ -0,0 +1,30 @@
+import torch
+
+numbers = [
+        9469287, 1271334, 6712289, 8381842, 2455118,
+        5992875, 4890524, 1425045, 4681945, 8022348,
+        6483223, 8081143, 6192770, 1779814, 4443234,
+        8546532, 8224679, 9222657, 9323386, 5363473,
+        8541011, 9356134, 7956588, 3696206, 8534543,
+        4446344, 1728483, 7451212, 7496930, 1966247
+    ]
+numbers_tensor = torch.tensor(numbers)
+
+interval = 17043521
+
+def range_to_binmatrix(start, end):
+    numbers = torch.arange(start, end, dtype=torch.int64)
+    powers_of_two = 2 ** torch.arange(29, -1, -1, dtype=torch.int64)
+    binary_matrix = (numbers.unsqueeze(1) & powers_of_two).ne(0).int()
+    return binary_matrix
+
+for i in range(0, 63):
+    start = i*interval
+    end = (i+1)*interval
+
+    binary_matrix = range_to_binmatrix(start, end)
+    subset_sums = torch.mm(binary_matrix.float(),numbers_tensor.unsqueeze(1).float()).squeeze()
+    torch.save(binary_matrix, f"chunks/{i+1}_binmatrix.pt") 
+    torch.save(subset_sums, f"chunks/{i+1}_sums.pt")
+
+    print(f"Completed chunk {i+1}/63 ||| {(i+1)/63 * 100:.2f}%")
