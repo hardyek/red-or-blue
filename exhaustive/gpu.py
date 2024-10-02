3f87867cdd52e7eaadb728aa05a3fe19e28b43dd
hardyek <hardyewk@gmail.com>
2024-10-02 20:58:11 +0100

    exhaustive optimisation started

--- exhaustive/gpu.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def gpu_calculate_sums(subsets, numbers,device):
-    numbers_tensor = torch.tensor(numbers, dtype=torch.int64, device=device)
-    subsets_tensor = torch.tensor(subsets, dtype=torch.int64, device=device)
-    return torch.sum(subsets_tensor * numbers_tensor, dim=1).cpu().numpy()
